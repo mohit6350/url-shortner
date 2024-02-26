e9c5ed46775f7530b35d803528ef6b3f24585322
Mohit khare <35894819+mohit6350@users.noreply.github.com>
2024-02-27 01:38:52 +0530

    init setup

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+app = Flask(__name__)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
